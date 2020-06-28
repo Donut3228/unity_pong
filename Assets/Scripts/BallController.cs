0ecd269867943dc2e1d3018cef3f96a7606362d2
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2020-06-29 01:55:34 +0300

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private bool hitWall = false;
+    private Vector2 dir;
+    void Start () {
+        dir = new Vector2 (0.01f, -0.01f);
+        Invoke("Move", 0.03f);
+    }
+
+    void Move () {
+        if (hitWall == true) {
+            Vector2 new_v = new Vector2(dir.x, dir.y * -1.0f);
+            dir = new_v;
+            hitWall = false;
+        }
+        transform.Translate (dir);
+        Invoke ("Move", 0.03f);
+    }
+
+    // void Update () {
+    //     if (hitWall == false) {
+    //         hitWall = true;
+    //     }
+    // }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+        hitWall = true;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Vector2 dir;
+
+    private bool hitWall = false;
+
+    void Start () {
+        speed = 0.03f;
+        Invoke ("Move", speed);
+    }
+
+    void Move () {
+        if (hitWall == true) {
+            dir = Vector2.zero;
+            hitWall = false;
+        }
+        transform.Translate (dir * 0.0375f);
+        Invoke ("Move", speed);
+    }
+
+    void Update () {
+        if (Input.GetKey (KeyCode.DownArrow))
+            dir = -Vector2.up;
+        else if (Input.GetKey (KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.name.StartsWith ("Background")) {
+            hitWall = true;
+        }
+    }
+}
